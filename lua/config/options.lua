f672ce5daa56292c2a82152a56bae24640ad8d0e
Johnathon Schultz <5789141+johnschultz@users.noreply.github.com>
2026-01-13 17:07:23 -0800

    Update to latest LazyVim structure with preserved customizations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,7 @@
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.autoformat = true
-vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
-vim.g.lazyvim_check_order = false -- Don't complain that we load java extras in jdtls plugin
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
